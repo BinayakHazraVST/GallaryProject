65a7512914eb8cd2344f5ffa8024322269ed1986
BINAYAK-HAZRA <binayak.hazra2029@vedamsot.org>
2026-04-10 03:50:45 +0530

    limit input feature added

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,37 @@ import ImageCard from './components/ImageCard'
 
 const App = () => {
 
+  const [finalLimit, setFinalLimit] = useState(8);
+  const [limit, setLimit] = useState(8);
   const [userArray, setUserArray] = useState([])
   const [page, setPage] = useState(1);
 
   useEffect(function () {
     const getData = async () => {
-      const response = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=12`);
+      const response = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=${finalLimit}`);
       setUserArray(response.data);
     }
 
     getData();
-  }, [page])
+  }, [page, finalLimit])
 
   return (
     <div>
       <h1>Gallary Project</h1>
+      <form className="setLimitForm">
+        <span>Set the limit of the Images</span>
+        <div>
+          <input type="Number"
+            onChange={(event) => {
+              setLimit(event.target.value)
+            }}
+            value={limit} />
+          <button onClick={(event) => {
+            event.preventDefault();
+            setFinalLimit(limit)
+          }}>Submit</button>
+        </div>
+      </form>
 
       <div className="gallaryInterface">
         {userArray.length == 0 ? <div className="loadingImageBox">Loading Images...</div> : (
